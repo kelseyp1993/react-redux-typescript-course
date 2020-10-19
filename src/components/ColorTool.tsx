e7932f8e482e8b610d24641c453350b70a0c4c35
kelsey.helling <kpritsker@gblsys.com>
2020-10-19 14:23:43 -0700

    Pass colors as props

--- src/components/ColorTool.tsx
+++ src/components/ColorTool.tsx
@@ -2,16 +2,11 @@ import React from 'react';
 
 import { Color } from '../models/colors';
 
-export function ColorTool() {
+export type ColorToolProps = {
+    colors: Color[];
+}
 
-    const colors: Color[] = [
-        {id:1, name:'blue'},
-        {id:1, name:'purple'},
-        {id:1, name:'teal'},
-        {id:1, name:'aqua'},
-    ];
-
-    // const colorListItems: colors.map(color => <li>colors.name</li>);
+export function ColorTool(props: ColorToolProps) {
 
     return (
         <>
@@ -19,7 +14,7 @@ export function ColorTool() {
                 <h1>Color Tool</h1>
             </header>
             <ul>
-                {colors.map(color => <li key={color.id}>colors.name</li>)}
+                {props.colors.map(color => <li key={color.id}>{color.name}</li>)}
             </ul>
         </>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,27 @@
 // default import
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
 // named import
 import { ColorTool } from './components/ColorTool';
 import { CarTool } from './components/CarTool';
+import { Color } from './models/colors';
 
-ReactDOM.render(
+const colorList: Color[] = [
+  {id:1, name:'blue'},
+  {id:2, name:'purple'},
+  {id:3, name:'teal'},
+  {id:4, name:'aqua'},
+];
+
+render(
   //-custom component, to reference the componenet we pass in the
   // function object r the class reference
   //-"HelloWorld" is not being invoked here, the reference to the "HelloWorld"
   // function is being passed in
   //- always start with an uppercase letter
   <>
-    <ColorTool />
+    <ColorTool colors={colorList}/>
     <CarTool />
   </>,
   document.querySelector('#root'),
