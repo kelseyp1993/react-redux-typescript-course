099ef0c87e28fea27f0cdab4abf63b388d03e366
kelsey.helling <kpritsker@gblsys.com>
2020-10-19 10:18:54 -0700

    Extracted HelloWorld into a seperate file

---
+++ src/components/HelloWorld.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function HelloWorld() {
+  // output to the console 
+  // console.log('hi');
+
+  // add a breakpoint that will then stop when the browser executes it
+  // debugger;
+
+  // intrinsic element, with ReactDOM, the HTML tags are all intrinsic
+  // reference the intrensic element using a string-based tag name
+  // JSX - starts with lower case letter
+  return <>
+      <h1>Hello, World!</h1>
+      <span>test</span>
+    </>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
+// default import
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function HelloWorld() {
-  // intrinsic element, with ReactDOM, the HTML tags are all intrinsic
-  // reference the intrensic element using a string-based tag name
-  // JSX - starts with lower case letter
-  return React.createElement('h1', null, 'Hello, World!');
-}
+// named import
+import { HelloWorld as HelloWorld2} from './components/HelloWorld';
 
 ReactDOM.render(
   //-custom component, to reference the componenet we pass in the
   // function object r the class reference
   //-"HelloWorld" is not being invoked here, the reference to the "HelloWorld"
   // function is being passed in
-  //- always start with an uppercase
-  React.createElement(HelloWorld),
+  //- always start with an uppercase letter
+  <HelloWorld2 />,
   document.querySelector('#root'),
 );
