85ce5191815d578a872a1c672e8be6b3f73812cf
kelsey.helling <kpritsker@gblsys.com>
2020-10-19 13:31:27 -0700

    Dynamic list example

--- src/components/ColorTool.tsx
+++ src/components/ColorTool.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 
+import { Color } from '../models/colors';
+
 export function ColorTool() {
-  // intrinsic element, with ReactDOM, the HTML tags are all intrinsic
-  // reference the intrensic element using a string-based tag name
-  // JSX - starts with lower case letter
-  return <>
-        <header>
-            <h1>Color Tool</h1>
-        </header>
-        <ul>
-            <li>blue</li>
-            <li>purple</li>
-            <li>teal</li>
-        </ul>
-    </>;
+
+    const colors: Color[] = [
+        {id:1, name:'blue'},
+        {id:1, name:'purple'},
+        {id:1, name:'teal'},
+        {id:1, name:'aqua'},
+    ];
+
+    // const colorListItems: colors.map(color => <li>colors.name</li>);
+
+    return (
+        <>
+            <header>
+                <h1>Color Tool</h1>
+            </header>
+            <ul>
+                {colors.map(color => <li key={color.id}>colors.name</li>)}
+            </ul>
+        </>
+    );
 }
