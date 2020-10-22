a6744eeeac1e3d5fa98f6951ef2926a0d4c3b7d0
kelsey.helling <kpritsker@gblsys.com>
2020-10-22 08:56:42 -0700

    Lecture Updates

--- src/containers/CalcToolContainer.ts
+++ src/containers/CalcToolContainer.ts
@@ -1,10 +1,10 @@
 import { connect } from 'react-redux';
-import { CalcToolState } from '../models/calc';
+import { CalcToolAppState } from '../models/calc';
 import { bindActionCreators, Dispatch } from 'redux';
 import { createAddAction, createSubtractAction, createMultiplyAction, createDivideAction } from '../actions/calcToolActions';
 import { CalcTool } from '../components/CalcTool';
 
-const mapStateToProps = (state: CalcToolState) => ({ result: state.result, history: state.history });
+const mapStateToProps = (state: CalcToolAppState) => ({ result: state.result, history: state.history });
 
 const mapDispatchToProps = (dispatch: Dispatch) =>
     bindActionCreators(
--- src/reducers/calcToolReducers.ts
+++ src/reducers/calcToolReducers.ts
@@ -1,54 +1,79 @@
-import { CalcToolState } from "../models/calc";
-import { AddAction, SubtractAction, ADD_ACTION, SUBTRACT_ACTION, MULTIPLY_ACTION, DIVIDE_ACTION, MultiplyAction, DivideAction } from "../actions/calcToolActions";
+import { Reducer, AnyAction, combineReducers } from 'redux';
+import { CalcToolAppState, HistoryEntry } from '../models/calc';
+import {
+    AddAction,
+    SubtractAction,
+    MultiplyAction,
+    DivideAction,
+    ADD_ACTION,
+    SUBTRACT_ACTION,
+    MULTIPLY_ACTION,
+    DIVIDE_ACTION,
+} from '../actions/calcToolActions';
 
-type CalcToolActions = AddAction | SubtractAction | MultiplyAction | DivideAction;
+type CalcToolActions =
+    | AddAction
+    | SubtractAction
+    | MultiplyAction
+    | DivideAction;
 
-export const calcToolReducer = (
-    state: CalcToolState = {result: 0, history: []}, 
-    action: CalcToolActions
-    ) => {
-        switch(action.type) {
-            case ADD_ACTION:
-                return{
-                    ...state,
-                    result: state.result + action.payload.value,
-                    history: [...state.history, {
-                        opName: '+',
-                        opValue: action.payload.value,
-                        id: Math.max(...state.history.map(he => he.id), 0) +1,
-                    }],
-                };
-            case SUBTRACT_ACTION: 
-                return{
-                    ...state,
-                    result: state.result - action.payload.value,
-                    history: [...state.history, {
-                        opName: '-',
-                        opValue: action.payload.value,
-                        id: Math.max(...state.history.map(he => he.id), 0) +1,
-                    }],
-                };
-            case MULTIPLY_ACTION: 
-                return{
-                    ...state,
-                    result: state.result * action.payload.value,
-                    history: [...state.history, {
-                        opName: '*',
-                        opValue: action.payload.value,
-                        id: Math.max(...state.history.map(he => he.id), 0) +1,
-                    }],
-                };
-            case DIVIDE_ACTION: 
-                return{
-                    ...state,
-                    result: state.result / action.payload.value,
-                    history: [...state.history, {
-                        opName: '/',
-                        opValue: action.payload.value,
-                        id: Math.max(...state.history.map(he => he.id), 0) +1,
-                    }],
-                };
-            default: 
-                return state;
-        }
-    };
+// Pure Function
+// - All data comes in through the parameters
+// - Parameters cannot be mutated
+// - No side effects can be performed
+// - The only output is the return value
+export const resultReducer: Reducer<number, CalcToolActions> = (
+    result = 0,
+    action,
+) => {
+    switch (action.type) {
+        case ADD_ACTION:
+            return result + action.payload.value;
+        case SUBTRACT_ACTION:
+            return result - action.payload.value;
+        case MULTIPLY_ACTION:
+            return result * action.payload.value;
+        case DIVIDE_ACTION:
+            return result / action.payload.value;
+        default:
+            return result;
+    }
+};
+
+export const historyReducer: Reducer<HistoryEntry[], CalcToolActions> = (
+    history = [],
+    action,
+) => {
+    if (
+        [ADD_ACTION, SUBTRACT_ACTION, MULTIPLY_ACTION, DIVIDE_ACTION].includes(
+            action.type,
+        )
+    ) {
+        return [
+            ...history,
+            {
+                opName: action.type,
+                opValue: action.payload.value,
+                id: Math.max(...history.map((he) => he.id), 0) + 1,
+            },
+        ];
+    }
+
+    return history;
+};
+
+// export const calcToolReducer: Reducer<CalcToolAppState, AnyAction> = (
+//   state = {} as CalcToolAppState,
+//   action,
+// ) => {
+//   return {
+//     ...state,
+//     result: resultReducer(state.result, action as CalcToolActions),
+//     history: historyReducer(state.history, action as CalcToolActions),
+//   };
+// };
+
+export const calcToolReducer = combineReducers({
+    result: resultReducer,
+    history: historyReducer,
+});
